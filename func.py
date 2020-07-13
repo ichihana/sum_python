77722f44f37c1219d85e7b00983bd1c627fed998
Miwa Ichihana <ichihana0517@icloud.com>
2020-07-13 09:19:11 +0900

    Add files via upload

---
+++ func.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def func(x):
+    s = np.sum(x)
+    return s
---
+++ sum_python.py
@@ -0,0 +1,6 @@
+import numpy as np
+from func import func
+
+x = np.array([1,2,3,4,4,5,6,7,8])
+s = func(x)
+print(s)
